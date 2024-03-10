69250a50664f0d77131f63dc8936b8ad79e44cfd
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-10 15:52:21 -0500

    /notes is now doing what it's supposed to!

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const express = require("express");
 const PORT = 3001;
 const app = express();
 
+// from 11-express/01-ins_setup: "Import built-in Node.js package 'path' to resolve path of files that are located on the server"
+const path = require('path');
+
 // db File connected
-const dbFile = require("./db/db.json");
+// const dbFile = require("./db/db.json");
 
 // directing things to the public folder
 app.use(express.static("public"));
 
 // get req /notes = res with the db file
-app.get("/notes", (req, res) => {res.json(dbFile)});
+app.get("/notes", (req, res) => { res.sendFile(path.join(__dirname, 'public/notes.html')); });
 
 
 app.listen(PORT, () =>
