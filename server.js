f5419b5c6624bb68581c5dce3582dd6734458917
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-10 15:43:23 -0500

    have a get request working

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const express = require("express");
 const PORT = 3001;
 const app = express();
 
+// db File connected
+const dbFile = require("./db/db.json");
 
+// directing things to the public folder
+app.use(express.static("public"));
 
-
+// get req /notes = res with the db file
+app.get("/notes", (req, res) => {res.json(dbFile)});
 
 
 app.listen(PORT, () =>
