879ce0d159a6b2abaf61402839bbfa9cd79727c6
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-10 19:08:32 -0500

    added helper

--- server.js
+++ server.js
@@ -3,12 +3,21 @@
 const express = require("express");
 const PORT = 3001;
 const app = express();
+// fs
+const fs = require("fs");
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
 
 // from 11-express/01-ins_setup: "Import built-in Node.js package 'path' to resolve path of files that are located on the server"
 const path = require('path');
 
 // db File connected
 const dbFile = require("./db/db.json");
+
+// middleware for parsing JSON
+app.use(express.json());
+// middleware for parsing URL encoded data
+app.use(express.urlencoded());
 
 // directing things to the public folder
 app.use(express.static("public"));
@@ -32,7 +41,22 @@ app.get("/api/notes", (req, res) => {
 
 // POST REQ
 app.post("/api/notes", (req, res) => {
+    // // Prepare a response object to send back to the client
+    // let response;
 
+    // // Check if there is anything in the response body
+    // if (req.body && req.body.product) {
+    //     response = {
+    //         status: 'success',
+    //         data: req.body,
+    //     };
+    //     res.json(`Review for ${response.data.product} has been added!`);
+    // } else {
+    //     res.json('Request body must at least contain a product name');
+    // }
+
+    // // Log the response body to the console
+    // console.log(req.body);
 });
 
 
@@ -42,5 +66,5 @@ app.post("/api/notes", (req, res) => {
 
 // from 11-express/01-ins_setup: "listen() method is responsible for listening for incoming connections on the specified port"
 app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT}`)
+    console.log(`App listening at http://localhost:${PORT}`)
 );
